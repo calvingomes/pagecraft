39718ad289be1dbb820e8b746cbb3043ffe88360
Calvin <34601104+calvingomes@users.noreply.github.com>
2026-01-25 19:57:21 +0400

    refactor: meta data for home page

---
+++ app/home/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+import { Navbar } from "@/components/layout/Navbar/Navbar";
+
+export const metadata: Metadata = {
+  title: "Home | PageCraft",
+  description:
+    "Build and share a beautiful single-page profile with PageCraft.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Navbar />
+      <body>{children}</body>
+    </html>
+  );
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,4 @@
-import type { Metadata } from "next";
 import { Header } from "@/components/layout/HomePageHeader/Header";
-
-export const metadata: Metadata = {
-  title: "Home | PageCraft",
-  description:
-    "Build and share a beautiful single-page profile with PageCraft.",
-};
 
 export default function HomePage() {
   return <Header />;
