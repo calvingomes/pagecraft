3047d30ea880f97558ac8aabcb9e4c0e97ae513c
Calvin <34601104+calvingomes@users.noreply.github.com>
2026-01-25 16:12:03 +0400

    update home page placeholder content

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <div>
       <main>
-        <div>Hello</div>
+        <div>
+          <h1>Welcome to PageCraft </h1>
+          <h3>Hello, This is the Home page</h3>
+        </div>
       </main>
     </div>
   );
