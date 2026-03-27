8fcf713a49ced5ea6fb76906dcd01709e4d46bda
Calvin <34601104+calvingomes@users.noreply.github.com>
2026-03-27 18:38:09 +0530

    feat: update footer

--- components/views/HomeLanding/sections/Footer/Footer.tsx
+++ components/views/HomeLanding/sections/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { ClaimInput } from "@/components/ui/ClaimInput/ClaimInput";
 import layoutStyles from "../../LandingLayout.module.css";
 import styles from "./Footer.module.css";
 
@@ -6,47 +7,27 @@ export function Footer() {
   return (
     <footer className={styles.footer} aria-label="Footer">
       <div className={`${layoutStyles.container} ${styles.footerContainer}`}>
-        
-        {/* Top Section: Heading & Links */}
+
+        {/* Top Section: Headline + ClaimInput */}
         <div className={styles.footerTop}>
           <h2 className={styles.footerHeadline}>
-            Experience the future<br />of page building.
+            Your link.<br />Your brand.
           </h2>
-
-          <div className={styles.footerLinksGrid}>
-            {/* Column 1 */}
-            <div className={styles.footerLinkColumn}>
-              <Link href="#" className={styles.footerLink}>Download</Link>
-              <Link href="#" className={styles.footerLink}>Product</Link>
-              <Link href="#" className={styles.footerLink}>Docs</Link>
-              <Link href="#" className={styles.footerLink}>Changelog</Link>
-            </div>
-
-            {/* Column 2 */}
-            <div className={styles.footerLinkColumn}>
-              <Link href="#" className={styles.footerLink}>Blog</Link>
-              <Link href="#" className={styles.footerLink}>Pricing</Link>
-              <Link href="#" className={styles.footerLink}>Use Cases</Link>
-              <Link href="/auth" className={styles.footerLink}>Sign In</Link>
-            </div>
+          <div className={styles.claimWrapper}>
+            <ClaimInput />
           </div>
         </div>
 
         {/* Middle Section: Big Brand Text */}
         <div className={styles.footerBrandContainer}>
-          <h1 className={styles.footerBrandText}>
+          <h3 className={styles.footerBrandText}>
             PageCraft
-          </h1>
+          </h3>
         </div>
 
-        {/* Bottom Section: Legal & Meta */}
+        {/* Bottom Section: Legal */}
         <div className={styles.footerBottom}>
-          <div className={styles.footerBottomLeft}>
-            <span className={styles.footerLogo}>PageCraft</span>
-            <Link href="#" className={styles.footerBottomLink}>About PageCraft</Link>
-            <Link href="#" className={styles.footerBottomLink}>Products</Link>
-          </div>
-          
+          <span className={styles.footerLogo}>PageCraft</span>
           <div className={styles.footerBottomRight}>
             <Link href="#" className={styles.footerBottomLink}>Privacy</Link>
             <Link href="#" className={styles.footerBottomLink}>Terms</Link>
