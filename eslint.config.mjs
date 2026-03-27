73fab75f51a064f37105de92b798692ed1c1f3bf
Calvin <34601104+calvingomes@users.noreply.github.com>
2026-03-28 01:06:59 +0530

    feat: add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import cssModules from "eslint-plugin-css-modules";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["**/*.tsx", "**/*.ts"],
+    plugins: {
+      "css-modules": cssModules,
+    },
+    rules: {
+      "css-modules/no-undef-class": ["warn", { camelCase: true }],
+      "css-modules/no-unused-class": ["warn", { camelCase: true }],
+    },
+  },
 ]);
 
 export default eslintConfig;
